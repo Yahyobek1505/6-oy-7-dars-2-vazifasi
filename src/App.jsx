380442d24a92c3fd35df804b5ffe547386f0b4c5
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-03-24 11:35:15 +0500

    Homework is almost done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
-import './App.css'
 import Header from './components/Header'
+import Card from './components/Card'
+import './App.css'
 
 function App() {
 
@@ -10,7 +11,12 @@ function App() {
     <Header></Header>
     <div className="container">    
 <div className="main">
-
+<Card></Card>
+<Card></Card>
+<Card></Card>
+<Card></Card>
+<Card></Card>
+<Card></Card>
 </div>
     </div>
     </>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,7 +2,19 @@
 import './index.css'
 function Card() {
   return (
-    <div>Contact</div>
+    <>
+    <div className="card">
+  <img src="https://www.cnet.com/a/img/resize/592e5101f4fee1caf72f9e0169c8784ddf9eb12a/hub/2023/05/04/31dfdcf2-1ac3-4320-b40c-4c356300f06e/google-pixel-7a-phone-14.jpg?auto=webp&fit=crop&height=576&width=768" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div className="row gap-2">
+    <a href="#" className="col-4 btn btn-primary">Update</a>
+    <a href="#" className="  col-4 btn btn-danger">Delete</a>
+    </div>
+  </div>
+</div>
+    </>
   )
 }
 
