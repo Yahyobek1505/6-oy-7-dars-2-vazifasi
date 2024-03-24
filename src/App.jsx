7ee9441f4d839c9b0c8c834fedca36b168d53ac2
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-03-24 23:53:46 +0500

    Homework is done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Header from "./components/Header";
 import Card from "./components/Card";
+import { FadeLoader } from "react-spinners";
 import "./App.css";
 
 function App() {
-  const [books, setBooks] = useState([]);
+  const [phones, setPhones] = useState([]);
   const [loading, setIsloading] = useState(false);
+  const [pending, setPending] = useState(false)
+  const nameRef = useRef("");
+  const priseRef = useRef(0);
+  const statusRef = useRef("active");
+  const descRef = useRef("");
+
   useEffect(() => {
+    setIsloading(true);
     fetch("https://auth-rg69.onrender.com/api/products/all", {
       method: "GET",
     })
       .then((res) => res.json())
       .then((data) => {
-        setBooks(data);
+        setPhones(data);
         console.log(data);
       })
       .catch((err) => {
@@ -22,44 +30,147 @@ function App() {
         setIsloading(false);
       });
   }, []);
+  function validation(nameRef, priseRef, statusRef, descRef) {
+    if (nameRef.current.value.trim() < 3) {
+      alert("Name is empty!");
+      nameRef.current.focus();
+      return false;
+    }
+    if (Number(priseRef.current.value.trim() > 1000000)) {
+      alert("Prise is not valid!");
+      priseRef.current.focus();
+      return false;
+    }
+    if (!statusRef.current.value) {
+      alert("Status is empty!");
+      statusRef.current.focus();
+      return false;
+    }
+    if (descRef.current.value.trim() < 3) {
+      alert("Description is empty!");
+      descRef.current.focus();
+      return false;
+    }
+    return true;
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    const isValid = validation(nameRef, priseRef, statusRef, descRef);
+    setPending(true);
+    if (isValid) {
+      const phone = {
+        name: nameRef.current.value,
+        description: nameRef.current.value,
+        status: statusRef.current.value,
+        price: priseRef.current.value,
+        category_id: 5,
+      };
+
+      fetch("https://auth-rg69.onrender.com/api/products ", {
+    method: "POST",
+    headers: {
+        "Content-type": "application/json",
+    },
+    body: JSON.stringify(phone),
+})
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        if (data.id) {
+            let copied = JSON.parse(JSON.stringify(phones));
+            copied.push(data);
+            setPhones(copied);
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    .finally(() => {
+      nameRef.current.value = "";
+      priseRef.current.value = "";
+      statusRef.current.value = "";
+      descRef.current.value = "";
+      setPending(false);
+    });
+    }
+  }
+  
+  function handleDelete(id) {
+    let isDelete = confirm("Rostdan ham ushbu ma'lumotni o'chirmoqchimisiz?")
+    if (isDelete && id) {
+      fetch(`https://auth-rg69.onrender.com/api/products/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+
+        if (data.message == "Mahsulot muvaffaqiyatli o'chirildi") {
+          let copied = JSON.parse(JSON.stringify(phones));
+         copied = copied.filter(el =>{
+            return el.id != id
+          })
+          setPhones(copied);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+  }
   return (
     <>
       <Header></Header>
       <div className="container">
         <form className="w-50 mx-auto d-flex flex-column mb-4 gap-2">
-          <label htmlFor="exampleFormControlInput1" className="form-label">
+          <label htmlFor="exampleFormControlInput2" className="form-label">
             Enter name
           </label>
           <input
+            ref={nameRef}
             type="text"
             className="form-control"
-            id="exampleFormControlInput1"
-            placeholder="Book name"
+            id="exampleFormControlInput2"
+            placeholder="Phone name"
           />
           <label htmlFor="exampleFormControlInput1" className="form-label">
             Price
           </label>
           <input
+            ref={priseRef}
             type="number"
             className="form-control"
             id="exampleFormControlInput1"
-            placeholder="Book price"
+            placeholder="Phone price"
           />
-          <select class="form-select" aria-label="Default select example">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Status
+          </label>
+          <select
+            ref={statusRef}
+            className="form-select"
+            aria-label="Default select example">
             <option value="Active">Active</option>
             <option value="Inactive">Inactive</option>
           </select>
           <textarea
-            class="form-control"
-            id="exampleFormControlTextarea1"
+            ref={descRef}
+            className="form-control"
+            id="exampleFormControlTextarea3"
             rows="2"
             placeholder="Any description . . . . ."></textarea>
-            <button className="btn btn-primary">Submit</button>
+          <button disabled = {pending ? true : false} onClick={handleClick} className="btn btn-primary">
+          {pending ? "Loading....." : "Send"}  
+          </button>
         </form>
         <div className="main">
-          {books.map((book, index) => {
-            return <Card></Card>;
-          })}
+          <div>
+            {loading && <FadeLoader color="lightGreen" size={50}></FadeLoader>}
+          </div>
+          {!loading &&
+            phones.map((el, index) => {
+              return <Card deleteItem = {handleDelete} key={index} phone={el}></Card>;
+            })}
         </div>
       </div>
     </>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,16 +1,20 @@
 
 import './index.css'
-function Card() {
+function Card(props) {
+  const {name, description, price, status, id} = props.phone;
+  const {deleteItem} = props
   return (
     <>
     <div className="card">
   <img src="https://www.cnet.com/a/img/resize/592e5101f4fee1caf72f9e0169c8784ddf9eb12a/hub/2023/05/04/31dfdcf2-1ac3-4320-b40c-4c356300f06e/google-pixel-7a-phone-14.jpg?auto=webp&fit=crop&height=576&width=768" className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h2 className="card-title">{name}</h2>
+<h4>{status}</h4>
+<p>${price}</p>
+    <p className="card-text">{description}</p>
     <div className="row gap-2">
     <a href="#" className="col-4 btn btn-primary">Update</a>
-    <a href="#" className="  col-4 btn btn-danger">Delete</a>
+    <a href="#" className="  col-4 btn btn-danger" onClick= {() => {deleteItem(id)}}>Delete</a>
     </div>
   </div>
 </div>
